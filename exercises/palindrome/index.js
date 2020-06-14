4f866fd7a75537b8e0d88deb5e9a05ff5157fabe
schreyerpeter <pschreyer@weedmaps.com>
2020-06-13 22:36:59 -0700

    Palindrome and reverseInt

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,8 +7,24 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+// function palindrome(str) {
+//   return str.split("").reverse().join("") === str;
+// }
+
+// function palindrome(str) {
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] !== str[str.length - 1 - i]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
 function palindrome(str) {
-  return str.split("").reverse().join("") === str;
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 module.exports = palindrome;
