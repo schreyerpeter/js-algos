4b9d0a4594a604188865d3ff43f82d07749372b1
schreyerpeter <pschreyer@weedmaps.com>
2020-06-16 13:12:07 -0700

    Anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,34 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//   function buildCharacterMap(string) {
+//     const map = {};
+
+//     for (let char of string.replace(/[^\w]/g, "").toLowerCase()) {
+//       map[char] = map[char] + 1 || 1;
+//     }
+//     return map;
+//   }
+
+//   const aMap = buildCharacterMap(stringA);
+//   const bMap = buildCharacterMap(stringB);
+
+//   if (Object.keys(aMap).length !== Object.keys(bMap).length) return false;
+
+//   for (let char in aMap) {
+//     if (aMap[char] !== bMap[char]) return false;
+//   }
+
+//   return true;
+// }
+
+function anagrams(stringA, stringB) {
+  function cleanString(string) {
+    return string.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+  }
+
+  return cleanString(stringA) === cleanString(stringB);
+}
 
 module.exports = anagrams;
