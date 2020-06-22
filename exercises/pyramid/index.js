428e8167c96d1db8dfd237d0b7ce19319bc8b75a
schreyerpeter <pschreyer@weedmaps.com>
2020-06-22 15:33:53 -0700

    Pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,12 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(
+      `${" ".repeat(n - i)}${"#".repeat(2 * i - 1)}${" ".repeat(n - i)}`,
+    );
+  }
+}
 
 module.exports = pyramid;
