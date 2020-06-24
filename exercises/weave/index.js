aa8cae4379e7ecd1bcd4efa9c045e76758c344a5
schreyerpeter <pschreyer@weedmaps.com>
2020-06-24 11:17:09 -0700

    Weave queues

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -22,8 +22,21 @@
 //    q.remove() // 2
 //    q.remove() // 'There'
 
-const Queue = require('./queue');
+const Queue = require("./queue");
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+  const weaved = new Queue();
+
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()) {
+      weaved.add(sourceOne.remove());
+    }
+    if (sourceTwo.peek()) {
+      weaved.add(sourceTwo.remove());
+    }
+  }
+
+  return weaved;
+}
 
 module.exports = weave;
