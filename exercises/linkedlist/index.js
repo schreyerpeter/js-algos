8378657283eb4e5fd75cb8c10e6e1949d58df564
schreyerpeter <pschreyer@weedmaps.com>
2020-06-25 19:07:19 -0700

    Complete linked lists

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -15,7 +15,7 @@ class LinkedList {
   }
 
   insertFirst(data) {
-    this.head = new Node(data, this.head);
+    this.insertAt(data, 0);
   }
 
   size() {
@@ -31,18 +31,20 @@ class LinkedList {
   }
 
   getFirst() {
-    return this.head;
+    return this.getAt(0);
   }
 
   getLast() {
-    let node = this.head;
+    // let node = this.head;
 
-    while (node) {
-      if (!node.next) return node;
-      node = node.next;
-    }
+    // while (node) {
+    //   if (!node.next) return node;
+    //   node = node.next;
+    // }
 
-    return node;
+    // return node;
+
+    return this.getAt(this.size() - 1);
   }
 
   clear() {
@@ -126,6 +128,24 @@ class LinkedList {
 
     const node = new Node(data, previous.next);
     previous.next = node;
+  }
+
+  forEach(fn) {
+    let node = this.head;
+    let counter = 0;
+    while (node) {
+      fn(node, counter);
+      node = node.next;
+      counter++;
+    }
+  }
+
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      yield node;
+      node = node.next;
+    }
   }
 }
 
