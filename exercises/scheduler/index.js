2d49358b21f7c13dd5e30c0c09ab820e96be0177
schreyerpeter <pschreyer@weedmaps.com>
2020-07-18 10:59:42 -0700

    Scheduler

---
+++ exercises/scheduler/index.js
@@ -0,0 +1,9 @@
+// Good morning! Here's your coding interview problem for today.
+
+// This problem was asked by Apple.
+
+// Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+
+function scheduler(f, n) {
+  setTimeout(fn, n);
+}
